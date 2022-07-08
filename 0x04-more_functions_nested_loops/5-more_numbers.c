33d44aab9f8d2390df1773351baa32bb3eebf46e
zyzer01 <davidicfola@gmail.com>
2022-07-07 20:14:29 -0700

    new commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,13 @@ void more_numbers(void);
 
 	for (i = 0; i < 10; i++)
 	{
-		if (j >= 10)
-			_putchar(j / 10 + '0');
-		_putchar(j % 10 + '0');
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
