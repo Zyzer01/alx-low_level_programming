49a7a50363580ac7273e57b98968c2095833a19c
zyzer01 <davidicfola@gmail.com>
2022-07-07 21:57:33 -0700

    new

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,25 +9,29 @@
  */
 int main(void)
 {
-	int i;
+	int n;
 
-	for (i = 1; i <= 100; i++)
+	for (n = 1; n <= 100; n++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
+		if (n == 1)
+			printf("%d", n);
+		else if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			printf("Fizz ");
-		} else if (i % 5 == 0 && i % 3 != 0)
+			printf(" FizzBuzz");
+		}
+		else if (n % 3 == 0)
 		{
-			printf("Buzz ");
-		} else if (i % 3 == 0 && i % 5 == 0)
+			printf(" Fizz");
+		}
+		else if (n % 5 == 0)
 		{
-			printf("FizzBuzz ");
-		} else
+			printf(" Buzz");
+		}
+		else
 		{
-			printf("%d ", i);
+			printf(" %d", n);
 		}
 	}
 	printf("\n");
-
 	return (0);
 }
